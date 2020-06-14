0c0040e0a175e6e45875f83c8f82049d68daea32
dipanshushora <deepanshushora@gmail.com>
2020-06-15 01:44:45 +0530

    menu color set and carousel and content updated

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,31 @@
 // navigation menu
 var navbar = document.querySelector(".navbar");
 
-var menuClick = document.querySelector(".menuClick");
+var menuOpen = document.querySelector(".menuOpen");
+
+var menuClose = document.querySelector(".closemenu");
 
 var togglemenu = false;
 
-function showMenu(){
-    if(!togglemenu){
-        navbar.classList.add("active");
-        menuClick.innerHTML="close";
-        togglemenu = true
-    }
-    else{
-        menuClick.innerHTML="menu";
-        navbar.classList.remove("active");
-        togglemenu = false
-    }
+function openMenu(){
+    navbar.classList.add("active");
+    // menuClick.innerHTML="close";
+    togglemenu = true
+    // if(!togglemenu){
+        
+    // }
+    // else{
+        
+    // }
 }
 
-menuClick.addEventListener("click",showMenu);
+function closeMenu(){
+    // menuClick.innerHTML="menu";
+    navbar.classList.remove("active");
+    togglemenu = false
+}
+menuOpen.addEventListener("click",openMenu);
+menuClose.addEventListener("click",closeMenu);
+
 
 // carousel js
-
